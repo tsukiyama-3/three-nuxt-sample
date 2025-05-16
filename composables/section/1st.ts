6c733f9483e53a90fe6f0fff5bc865dd47a3352d
tsukiyama-3 <tsukiyama.mail@gmail.com>
2025-05-16 23:14:07 +0900

    add: 004

--- composables/section/1st.ts
+++ composables/section/1st.ts
@@ -80,3 +80,40 @@ export const use003 = (container: Ref<HTMLElement>) => {
 
   return { render };
 };
+
+export const use004 = () => {
+  if (!process.client) {
+    return { render: () => {} };
+  }
+  // scene
+  const { scene } = useScene();
+  // renderer
+  const { renderer } = useRenderer();
+  // camera
+  const { camera } = useCamera();
+  // geometry
+  const { geometry } = useGeometry();
+  // material
+  const { material } = useMaterial();
+  // mesh
+  const { mesh: box } = useMesh(geometry, material);
+  scene.add(box);
+  // controls
+  const { controls } = useControls(camera, renderer.domElement);
+  // axes
+  const { axesHelper } = useAxesHelper(5.0);
+  scene.add(axesHelper);
+  const isKeydown = ref(false);
+
+  // render
+  const render = () => {
+    requestAnimationFrame(render);
+    controls.update();
+    if (isKeydown.value) {
+      box.rotation.y += 0.05;
+    }
+    renderer.render(scene, camera);
+  };
+
+  return { render, renderer, isKeydown };
+};
